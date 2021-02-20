59ac9e832e3b08a9272ace21c7612e4c1fb0bed3
gqinghua <923063647@qq.com>
2021-02-20 10:08:47 +0800

    2021/02/20 add

---
+++ src/handlers/addUser.rs
@@ -0,0 +1,17 @@
+use crate::{db::addUser};
+use actix_web::{web, Error, HttpResponse};
+use deadpool_postgres::{Client, Pool};
+use crate::models::models::User;
+use crate::{ errors::errors::MyError};
+pub async fn add_user(
+    user: web::Json<User>,
+    db_pool: web::Data<Pool>,
+) -> Result<HttpResponse, Error> {
+    let user_info: User = user.into_inner();
+
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+
+    let new_user = addUser::add_user(&client, user_info).await?;
+
+    Ok(HttpResponse::Ok().json(new_user))
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use crate::handlers::addUser::add_user;
+use tokio_postgres::NoTls;
+
+pub mod models;
+pub mod handlers;
+pub mod db;
+pub mod Config;
+pub mod  errors;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let config = crate::Config::config::Config::from_env().unwrap();
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(web::resource("/users").route(web::post().to(add_user)))
+    })
+        .bind(config.server_addr.clone())?
+        .run();
+    println!("Server running at http://{}/", config.server_addr);
+
+    server.await
+}
